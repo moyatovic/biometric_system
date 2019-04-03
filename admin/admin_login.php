ce46809706ecbc7f5211ab4713a150a7cf95dd21
Badekale Adeleke <badekaleadeleke@gmail.com>
2019-04-03 14:51:22 +0100

    cors error resolved

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -1,10 +1,34 @@
 <?php
-session_start();
+function cors() {
 
+   // Allow from any origin
+   if (isset($_SERVER['HTTP_ORIGIN'])) {
+       // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+       // you want to allow, and if so:
+       header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+       header('Access-Control-Allow-Credentials: true');
+       header('Access-Control-Max-Age: 86400');    // cache for 1 day
+   }
+
+   // Access-Control headers are received during OPTIONS requests
+   if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+       if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+           // may also be using PUT, PATCH, HEAD etc
+           header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+       if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+           header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+       exit(0);
+   }
+}
+
+cors();
+session_start();
 
       // connection to database file
         require('../db/dbconn.php');
-
 
       //sql statement to pull username and password from admin table
         $query = "SELECT * FROM admins"; 
@@ -19,31 +43,20 @@ session_start();
       $user = $data['username'];
       $pass = $data['password'];
 
-
-        
-      //verify that the username and password tally with that in the database
-      if($username == $user && $password == $pass){  
-        
+       //verify that the username and password tally with that in the database
+      if($username == $user && $password == $pass){          
           $_SESSION['name'] = $user;
-         echo  json_encode($_SESSION['name']);
-            http_response_code(200);           
-
+          echo  json_encode($_SESSION['name']);
+          header( "HTTP/1.1 200 OK" );
           echo "{\"success\": true, \"message\": signed in}";
-        exit;
-          
-      
-     
+        exit;     
       }
-      else
-      { 
-         
-          header( "HTTP/1.1 204 invalid credentials" );
-        echo "{
+      else{ 
+          echo "{
           \"success\": false, \"message\": Incorrect username or Password
         }";
-       
-
+      http_response_code(401);
           exit;
-                }
-              
+                
+              }
       ?>
--- dashboard/add_employee.php
+++ dashboard/add_employee.php
@@ -1,24 +1,36 @@
 <?php
   session_start();
-  require('isloggedin.php');
+ // require('isloggedin.php');
   require('../db/dbconn.php');
 
 // fetching encoded json data from angular
   $data = json_decode(file_get_contents("php://input"), TRUE);
-  $firstname = $data['firstname'];
-  $lastname = $data['lastname'];
-  $middlename = $data ['lastname'];
-  $dob = $data['dob'];
-  $gender = $data['gender'];
-  $marital = $data['marital'];
-  $address = $data['address'];
-  $phone = $data['phone'];
-  $joinedDate = $data['joinedDate'];
-  $department  = $data['department'];
-  $role = $data['role'];
-  $employmentType = $data['employmentType'];
-  $createdBy = $data['createdBy'];
+  // $firstname = $data['firstname'];
+  // $lastname = $data['lastname'];
+  // $middlename = $data ['lastname'];
+  // $dob = $data['dob'];
+  // $gender = $data['gender'];
+  // $marital = $data['marital'];
+  // $address = $data['address'];
+  // $phone = $data['phone'];
+  // $joinedDate = $data['joinedDate'];
+  // $department  = $data['department'];
+  // $role = $data['role'];
+  // $employmentType = $data['employmentType'];
+  // $createdBy = $data['createdBy'];
 
+  $firstname = "adeleke"; 
+  $lastname = "badekale";
+  $middlename= "michael" ;
+  $dob= "08/08/1996"; 
+  $gender="male";
+  $marital="single";
+   $address = "G12, Pinnock Beach Estate, osapa london, lagos"; $phone="07017363892"; 
+   $joinedDate="03/07/2018"; 
+   $department= "Technology";
+    $role= "intern";
+    $employmentType = "contract";
+    $createdBy= "HR"; 
 
   $query = "INSERT INTO employee
             VALUES($firstname, $lastname, $middlename, $dob, $gender,$marital, $address, $phone, $joinedDate, $department, $role, $employmentType, $createdBy)" or die(mysql_error());
