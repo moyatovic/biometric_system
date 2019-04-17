f7d90295e4ec8efbbcf9d513e697e5c51a322374
Badekale Adeleke <badekaleadeleke@gmail.com>
2019-04-17 11:51:59 +0100

    employee report completed

--- report/daily_report.php
+++ report/daily_report.php
@@ -4,13 +4,13 @@
 
   $data = json_decode(file_get_contents("PHP://input"),true);
   $date =  date('Y-m-d', strtotime(mysqli_real_escape_string($conn, $data['date']))); 
-
+  
   $employees = [];
 
   //select the User id that exist in timesheet table 
   $sel_query = "SELECT employee.firstname, employee.lastname, department.department_name, timesheet.time_in, time_out, timesheet.entry_date FROM timesheet  
   INNER JOIN employee  ON timesheet.employee_id = employee.id
-  INNER JOIN department ON employee.department = department.id WHERE timesheet.entry_date = '2019-04-10' ORDER BY employee_id";
+  INNER JOIN department ON employee.department = department.id WHERE timesheet.entry_date = '".$date."'  ORDER BY employee~_id";
   
   
   $stmt = $conn->prepare($sel_query);
